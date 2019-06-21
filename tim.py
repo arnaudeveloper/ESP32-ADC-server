931062a8ef5fe592a55e57d1653bb9a597337acf
arnaudeveloper <38794634+arnaudeveloper@users.noreply.github.com>
2019-06-21 11:49:45 +0200

    Update tim.py

--- tim.py
+++ tim.py
@@ -1,41 +1,43 @@
-#Work
+#Universitat de Barcelona. Domotica
+#2019
+#Authors:
+#         Arnau Vicente Puiggros - GitHub: @arnaudeveloper
+#         Alvaro Baucells Costa
+
+#Codi TIM
 
 import network
 import machine
 import socket
 import utime
 
-host='192.168.4.1'
-port=2000
+#---Variables-----------------------------------------------
+
+host='192.168.4.1'                                          #necesitem ip del NCAP, que al primer socket actua com a servidor (host) 
+port=2000                                                   #port del primer socket
 s=None
 data=''
-
-
-ssid = 'NCAP'
+ssid = 'NCAP'                                               #dades de la xarxa del NCAP
 password = 'esp32ibz'
+
+#---Funcions------------------------------------------------
 
 def Enviem_ip_al_NCAP(ip):
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     #Crea un socket
 
   s.connect((host,port))                                    #Ens connectem al port 2000 del NCAP (host)
-  
-  msg=ip                                                    #Creem unmissatge amb la nostra ip
-  print("DEBUG===================>>>")
+  msg=ip                                                    #Creem un missatge amb la nostra ip, perque NCAP ens pogui identificar
   s.send(msg)                                               #Enviem el missatge pel port 
-  print("DEBUG===================>>>")
-
   data = s.recv(1024)                                       #Capturem la resposta del NCAP pel port 2000
   
-  
-  print("Data from NCAP:",data)                              #Prinetm la resposta
+  print("Data from NCAP:",data)                             #Printem la resposta
   
   s.close()                                                 #Tanquem el socket
-  
-  utime.sleep_ms(5000)                                      #Delay
+  utime.sleep_ms(5000)                                      #Delay 
 
 
 def Ens_conectem_a_la_red(ssid,password):
-  station.connect(ssid, password)                         #Connecta amb el ssid i el passwd
+  station.connect(ssid, password)                           #Connecta amb el ssid i el passwd
 
   while station.isconnected() == False:
     pass
@@ -45,61 +47,45 @@ def Ens_conectem_a_la_red(ssid,password):
   
 
 
-#---Codi-----
-#-------
-station = network.WLAN(network.STA_IF)
+#---Codi---------------------------------------------------
 
-station.active(True)
-
+station = network.WLAN(network.STA_IF)          #el TIM el configurem com a estacio, que accedeix a la xarxa del NCAP
+station.active(True)                            
 Ens_conectem_a_la_red(ssid,password)
 
 
 try:
   
-  ip=station.ifconfig()[0]                                 #get ip addr
-  
+  ip=station.ifconfig()[0]                      #agafem la nostra ip per obrir el segon socket (ara com servidor)
   print('ip:', ip)
   
   Enviem_ip_al_NCAP(ip)
 
-  #Configurem_el_socket_com_servidor(ip,port=2001)
-  #Configuraci
-  s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #Socket del port 20001. Servidor
   
+  #Configuracio del segon socket com a servidor
+  
+  s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #Es crea el segon socket (utilitzarem port 2005)
   s2.bind((ip,2005))                                      #Enllecem la nostra ip amb el socket que obrirem per escoltar (Servidor)
-  s2.listen(5)                                            #Limit de clients
+  s2.listen(5)                                            #Limit de requests que acceptara
 
   while True:
     
-    conn, addr = s2.accept()
+    conn, addr = s2.accept()         #estableix connexio quan rep un request, i guarda la direccio del client
     print('Got a connection from %s' % str(addr))
 
-    request = conn.recv(1024)
+    request = conn.recv(1024)        #rep missatge del client, amb un limit de 1024 bytes
     request = str(request)
-    #Analitzar el request
-    
     print('Content = %s' % request)  #Printa tot el que ha capturat
     
-    #En funcio del request contestem
-    response = "DATA"     #dades que ens ha demenat
-    
-    conn.sendall(response)#Enviem la resposta
-    conn.close()#Tanquem connexio per esperar a la seguent
+    response = "DATA"                #missatge que li tornem al client (en funcio del que demani)
+    conn.sendall(response)           #Enviem la resposta
+    conn.close()                     #Tanquem connexio per esperar la seguent connexio (es crea a l'inici del while)
       
     
-except:
-  if (s2):
-    s2.close()
-
-  
-  station.disconnect()
+except:                              #si es para el programa de manera externa, s'ha de tancar el socket
+  if (s2):    
+    s2.close()                       #si esta obert el socket, el tanca per evitar problemes a l'hora de tornar-lo a obrir en un futur
+  station.disconnect()               #si s'ha parat el programa, es desconecta de la xarxa
   station.active(False)
-
-
-
-
-
-
-
 
 
