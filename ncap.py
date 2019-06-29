674b46d288d54fcd9c00c09968db7d2bf2475ad2
arnaudeveloper <38794634+arnaudeveloper@users.noreply.github.com>
2019-06-29 12:30:31 +0200

    Update ncap.py

--- ncap.py
+++ ncap.py
@@ -1,3 +1,4 @@
+
 #Universitat de Barcelona. Domotica
 #2019
 #Authors:
@@ -8,7 +9,7 @@ import network
 import machine
 import socket
 import utime
-
+import struct
 
 s=None
 data=''
@@ -198,52 +199,56 @@ while count<1:            #Aquest while estara actiu fins que tots els TIMs s'ha
 #A partir d'aquest punt l'NCAP tanca el socket 2000, i  actua com a client
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+'''s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((ip, 80))
-s.listen(5)
-
-while True:
+s.listen(5)'''
+try:
   
-  conn, addr = s.accept()
-  print('Got a connection from %s' % str(addr))
+  while True:
 
-  request = conn.recv(1024)
-  request = str(request)
-  print('Content = %s' % request)  #Printa tot el que ha capturat
   
-  if 'firstname' in request:
-    name = request.split('=')[1]
-  else:
-    name = 'No name'
+    '''conn, addr = s.accept()
+    print('Got a connection from %s' % str(addr))
+  
+    request = conn.recv(1024)
+    request = str(request)
+    print('Content = %s' % request)  #Printa tot el que ha capturat
     
-  #Afegint box funciona??
-
-  #second_value_html = request.find('/?firstname')
-  #second_value_html = request.find('/?value')
-
-
-  #name = value_html.split('=')[1]
-  print('Aqui va el que he capturat----->>>')
-  #print(second_value_html)
-  msg=name.split()[0]
-  print(name.split()[0])
+    if 'firstname' in request:
+      name = request.split('=')[1]
+    else:
+      name = 'No name'
+      
+    #Afegint box funciona??
   
-
-
-  response = web_page()
-  conn.send('HTTP/1.1 200 OK\n')
-  conn.send('Content-Type: text/html\n')
-  conn.send('Connection: close\n\n')
-  conn.sendall(response)
-  conn.close()
-  
-  Envia_comanda_al_TIM(ip_TIM,msg)
+    #second_value_html = request.find('/?firstname')
+    #second_value_html = request.find('/?value')
   
   
-
+    #name = value_html.split('=')[1]
+    print('Aqui va el que he capturat----->>>')
+    #print(second_value_html)
+    msg=name.split()[0]
+    print(name.split()[0])
     
-
-
-
- 
-
+  
+  
+    response = web_page()
+    conn.send('HTTP/1.1 200 OK\n')
+    conn.send('Content-Type: text/html\n')
+    conn.send('Connection: close\n\n')
+    conn.sendall(response)
+    conn.close()'''
+    #debug
+    msg=128
+    env=struct.pack('h', msg)
+    
+    
+    Envia_comanda_al_TIM(ip_TIM,env)
+   
+except:
+  if (s):    
+    s.close()                       #si esta obert el socket, el tanca per evitar problemes a l'hora de tornar-lo a obrir en un futur
+  ap.disconnect()               #si s'ha parat el programa, es desconecta de la xarxa
+  ap.active(False)
+    
